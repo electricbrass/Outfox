c45b446cfc6c881e49ff85878aa75397320c8fa5
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2022-05-30 02:57:42 -0400

    Initial commit

---
+++ app/src/main/java/com/miamc/outfox/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.miamc.outfox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+fun mainMenu() {
+
+}
+
+fun startGame {
+    // change activity and setup game state
+}
